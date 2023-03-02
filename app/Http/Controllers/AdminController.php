a133b84ded468ea02d04d4404d6bbc352f8b5696
Muna Gautam <munagautam7796@gmail.com>
2023-03-02 19:48:05 +0545

    ...

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -210,4 +210,14 @@ class AdminController extends Controller
         }
         return redirect()->back()->with('status', 'Response send successfully');
     }
+    public function getDeleteDonner (donner $donner){
+        
+        $donner->deleted = 'Y';
+        $donner->save();
+
+        return redirect()->back()->with('message', 'Product Delete Sucess');
+    }
+
 }
+ 
+
